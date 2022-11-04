c0c177e6a991e26a8c1fcafe87f33a97dac88ee8
Eleli-Ayub <lesosioayub@gmail.com>
2022-11-04 16:48:02 +0300

    Modified header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../Styles/Header.scss";
+import stomerz from "../Assets/stomerz_logo.png";
 function Header() {
   return (
     <div className="headerWrapper">
+      <img src={stomerz} alt="" />
       <Link to="/">Home</Link>
       <Link to="Team">Team</Link>
       <Link to="Games">Upcoming Games</Link>
