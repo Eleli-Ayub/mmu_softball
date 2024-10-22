5c61e1a549696ab917fa308bd2d1aa21ac5875d7
Kamah <kamah@localhost.localdomain>
2024-10-22 18:27:48 +0300

    fresh new changes

--- src/Pages/about.tsx
+++ src/Pages/about.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About:React.FC = () => {
   return (
     <div className='h-96 w-full bg-green-600'>
-      About
+      About page under construction
       </div>
   )
 }
